042621ede25b00dd4f7d446bd4ed4aaa5bd1af7f
ShashwatPrasad <88634190+ShashwatPrasad@users.noreply.github.com>
2021-08-11 12:57:30 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 app.get('/listapp', function (req, res) {
-   fs.readFile( '/root/NJS/app.json', 'utf8', function (err, data) {
+   fs.readFile( '/home/runner/work/SHASH-ANZX-APP/SHASH-ANZX-APP/app.json', 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
    });
